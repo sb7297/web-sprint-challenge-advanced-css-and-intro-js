b11be276550213a6f7fe45f6190a2a32cc5ae61c
sb7297 <sbaireddy7297@gmail.com>
2021-06-11 12:41:56 -0400

    index.js: complete task 3

--- index.js
+++ index.js
@@ -228,8 +228,8 @@ artists[8].name = "Vincent Van Gogh";
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(artists, index) {
+  return `the artist at index ${index} is ${artists[index].name}`;
 }  
 
 
